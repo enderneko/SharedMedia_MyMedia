244d7724a1a2886f1311044eaa787260578f24b8
enderneko <enderneko-dev@outlook.com>
2024-05-15 14:45:10 +0800

    Initial commit

---
+++ MyMedia.lua
@@ -0,0 +1,62 @@
+local LSM = LibStub("LibSharedMedia-3.0") 
+
+-- ----------------------------------------------------------------------- --
+--                                BACKGROUND                               --
+-- ----------------------------------------------------------------------- --
+LSM:Register("background", "nyan", [[Interface\Addons\SharedMedia_MyMedia\background\nyan.tga]])
+LSM:Register("background", "Overlayrect", [[Interface\Addons\SharedMedia_MyMedia\background\Overlayrect.tga]])
+LSM:Register("background", "rectshadow", [[Interface\Addons\SharedMedia_MyMedia\background\rectshadow.tga]]) 
+LSM:Register("background", "screenoverlay", [[Interface\Addons\SharedMedia_MyMedia\background\screenoverlay.tga]]) 
+LSM:Register("background", "shadow square", [[Interface\Addons\SharedMedia_MyMedia\background\shadow square.tga]]) 
+
+
+-- ----------------------------------------------------------------------- --
+--                                  BORDER                                 --
+-- ----------------------------------------------------------------------- --
+LSM:Register ("border", "1 Pixel", [[Interface\Buttons\WHITE8X8]])
+
+
+-- ----------------------------------------------------------------------- --
+--                                   FONT                                  --
+-- ----------------------------------------------------------------------- --
+LSM:Register("font", "Accidental Presidency", [[Interface\Addons\SharedMedia_MyMedia\font\Accidental_Presidency.ttf]], 255) 
+LSM:Register("font", "Action Man", [[Interface\Addons\SharedMedia_MyMedia\font\Action_Man.ttf]], 255)
+LSM:Register("font", "Arial Black", [[Interface\Addons\SharedMedia_MyMedia\font\ArialBlack.ttf]], 255)
+LSM:Register("font", "BADABB", [[Interface\Addons\SharedMedia_MyMedia\font\BADABB.ttf]], 255)
+LSM:Register("font", "CalibriYahei", [[Interface\Addons\SharedMedia_MyMedia\font\CalibriYahei.ttf]], 255)
+LSM:Register("font", "coolvetica rg", [[Interface\Addons\SharedMedia_MyMedia\font\coolvetica_rg.ttf]], 255)
+LSM:Register("font", "DieDieDie", [[Interface\Addons\SharedMedia_MyMedia\font\DieDieDie.ttf]], 255) 
+LSM:Register("font", "Dolphin", [[Interface\Addons\SharedMedia_MyMedia\font\Dolphin.ttf]], 255) 
+LSM:Register("font", "Emblem", [[Interface\Addons\SharedMedia_MyMedia\font\Emblem.ttf]], 255) 
+LSM:Register("font", "Expressway", [[Interface\Addons\SharedMedia_MyMedia\font\Expressway.ttf]], 255)
+LSM:Register("font", "Fira Mono Medium", [[Interface\Addons\SharedMedia_MyMedia\font\FiraMono-Medium.ttf]], 255)
+LSM:Register("font", "Futura", [[Interface\Addons\SharedMedia_MyMedia\font\Futura.ttf]], 255)
+LSM:Register("font", "impact", [[Interface\Addons\SharedMedia_MyMedia\font\impact.ttf]], 255) 
+-- LSM:Register("font", "Invisible", [[Interface\Addons\SharedMedia_MyMedia\font\Invisible.ttf]], 255)
+LSM:Register("font", "LiberationSans-Regular", [[Interface\Addons\SharedMedia_MyMedia\font\LiberationSans-Regular.ttf]], 255)
+LSM:Register("font", "MagistralTT", [[Interface\Addons\SharedMedia_MyMedia\font\MagistralTT.ttf]], 255) 
+LSM:Register("font", "PEPSI (Modified)", [[Interface\Addons\SharedMedia_MyMedia\font\PEPSI (Modified).ttf]], 255) 
+LSM:Register("font", "PT Sans Narrow", [[Interface\Addons\SharedMedia_MyMedia\font\PT_Sans_Narrow.ttf]], 255) 
+LSM:Register("font", "RussoOne", [[Interface\Addons\SharedMedia_MyMedia\font\RussoOne.ttf]], 255) 
+LSM:Register("font", "SFWonderComic", [[Interface\Addons\SharedMedia_MyMedia\font\SFWonderComic.ttf]], 255)
+LSM:Register("font", "Ubuntu-C", [[Interface\Addons\SharedMedia_MyMedia\font\Ubuntu-C.ttf]], 255)
+LSM:Register("font", "visitor 1", [[Interface\Addons\SharedMedia_MyMedia\font\visitor_1.ttf]], 255) 
+LSM:Register("font", "visitor 2", [[Interface\Addons\SharedMedia_MyMedia\font\visitor_2.ttf]], 255) 
+LSM:Register("font", "wendy", [[Interface\Addons\SharedMedia_MyMedia\font\wendy.ttf]], 255)
+LSM:Register("font", "YanoneKaffeesatz-Regular", [[Interface\Addons\SharedMedia_MyMedia\font\YanoneKaffeesatz-Regular.ttf]], 255)
+
+
+-- ----------------------------------------------------------------------- --
+--                                  SOUND                                  --
+-- ----------------------------------------------------------------------- --
+LSM:Register("sound", "oya", [[Interface\Addons\SharedMedia_MyMedia\sound\oya.wav]]) 
+
+
+-- ----------------------------------------------------------------------- --
+--                                STATUSBAR                                --
+-- ----------------------------------------------------------------------- --
+LSM:Register("statusbar", "BuiOnePixel", [[Interface\Addons\SharedMedia_MyMedia\statusbar\BuiOnePixel.tga]]) 
+LSM:Register("statusbar", "fer25", [[Interface\Addons\SharedMedia_MyMedia\statusbar\fer25.tga]]) 
+LSM:Register("statusbar", "fer37", [[Interface\Addons\SharedMedia_MyMedia\statusbar\fer37.tga]])
+LSM:Register("statusbar", "nyanbar", [[Interface\Addons\SharedMedia_MyMedia\statusbar\nyanbar.tga]])
+LSM:Register("statusbar", "bar-bg", [[Interface\Addons\SharedMedia_MyMedia\statusbar\bar-bg.tga]]) 
